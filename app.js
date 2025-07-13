7173396ae2b273298cd0c7c30a8cc4ca663cfb51
Abdur Rahmaan Basra <abdurrehmanbasra1@gmail.com>
2025-07-14 03:39:01 +0500

    .........

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// ✅ Toggle hamburger menu
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
